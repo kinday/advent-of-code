17924daf5627015c52b2c8a87d986f7a0685b224
Leonard Kinday <leonard@kinday.ru>
2022-12-02 23:40:27 +0100

    Cache fetched input

--- advent-of-code-2022/src/main.rs
+++ advent-of-code-2022/src/main.rs
@@ -5,6 +5,8 @@ use std::io::{self, prelude::*};
 mod day_1;
 mod day_2;
 
+const CACHEDIR: &'static str = ".cache";
+
 struct Cli {
     day: i8,
     filename: Option<std::path::PathBuf>,
@@ -35,6 +37,18 @@ fn read_input_from_file(file_path: std::path::PathBuf) -> io::Result<Vec<String>
     return Ok(result);
 }
 
+fn write_input_to_file(file_path: std::path::PathBuf, input: &String) -> Result<(), io::Error> {
+    fs::create_dir_all(CACHEDIR)?;
+
+    let file = fs::File::create(file_path)?;
+    let mut file = io::LineWriter::new(file);
+
+    file.write_all(input.as_bytes())?;
+    file.flush()?;
+
+    Ok(())
+}
+
 fn read_input_from_stdin() -> io::Result<Vec<String>> {
     let lines = io::stdin().lines();
     let mut result = Vec::new();
@@ -50,6 +64,17 @@ fn read_input_from_stdin() -> io::Result<Vec<String>> {
 }
 
 fn read_input_from_remote(day: String) -> Result<Vec<String>, ureq::Error> {
+    let file_path = std::path::PathBuf::from(format!("{}/day_{}", CACHEDIR, day));
+
+    #[cfg(debug_assertions)]
+    eprintln!("Using {} as cache", file_path.to_string_lossy());
+
+    if let Ok(cached_input) = read_input_from_file(file_path.clone()) {
+        #[cfg(debug_assertions)]
+        eprintln!("Found day {} in cache", day);
+        return Ok(cached_input);
+    };
+
     let token = env::var("AOC_TOKEN").expect("no $AOC_TOKEN defined");
     let path = format!("https://adventofcode.com/2022/day/{}/input", day);
     let cookie = format!("session={}", token);
@@ -57,6 +82,11 @@ fn read_input_from_remote(day: String) -> Result<Vec<String>, ureq::Error> {
         .set("Cookie", &cookie)
         .call()?
         .into_string()?;
+
+    if let Err(error) = write_input_to_file(file_path, &body) {
+        eprintln!("Could not save day {} to cache", day);
+        eprintln!("{}", error);
+    };
 
     let mut result = Vec::new();
     for line in body.lines() {
