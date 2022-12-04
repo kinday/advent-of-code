c73d8fc4ebb29359489c3eaf8ef6b7bba6e94d51
Leonard Kinday <leonard@kinday.ru>
2022-12-04 15:16:24 +0100

    Refactor 2022/4
    Clean up unnecessary variables, return consistently.

--- advent-of-code-2022/src/day_4.rs
+++ advent-of-code-2022/src/day_4.rs
@@ -24,33 +24,35 @@ impl From<String> for Assignment {
             .expect("no right bound")
             .parse::<i32>()
             .expect("no valid right bound");
+
         Assignment { left, right }
     }
 }
 
 impl From<&String> for Pair {
     fn from(s: &String) -> Pair {
-        let raw_assignments = s.split(",").take(2);
-        let mut assignments = raw_assignments.map(|a| Assignment::from(a.to_string()));
+        let mut assignments = s
+            .split(",")
+            .take(2)
+            .map(|a| Assignment::from(a.to_string()));
         let assignment_1 = assignments.next().expect("no left assignment found");
         let assignment_2 = assignments.next().expect("no right assignment found");
-        return Pair(assignment_1, assignment_2);
+
+        Pair(assignment_1, assignment_2)
     }
 }
 
 impl Overlap for Pair {
     fn is_intersecting(&self) -> bool {
-        let range_a = self.0.left..=self.0.right;
-        let assignment_a: HashSet<i32> = range_a.collect();
-        let range_b = self.1.left..=self.1.right;
-        let assignment_b: HashSet<i32> = range_b.collect();
+        let assignment_a: HashSet<i32> = (self.0.left..=self.0.right).collect();
+        let assignment_b: HashSet<i32> = (self.1.left..=self.1.right).collect();
+
         assignment_a.intersection(&assignment_b).count().gt(&0)
     }
     fn is_concealing(&self) -> bool {
-        let range_a = self.0.left..=self.0.right;
-        let assignment_a: HashSet<i32> = range_a.collect();
-        let range_b = self.1.left..=self.1.right;
-        let assignment_b: HashSet<i32> = range_b.collect();
+        let assignment_a: HashSet<i32> = (self.0.left..=self.0.right).collect();
+        let assignment_b: HashSet<i32> = (self.1.left..=self.1.right).collect();
+
         assignment_a.is_subset(&assignment_b) || assignment_a.is_superset(&assignment_b)
     }
 }
@@ -60,20 +62,21 @@ fn parse_input(data: Vec<String>) -> Vec<Pair> {
 }
 
 fn solve_first(input: &Vec<Pair>) -> String {
-    let result = input
+    input
         .iter()
-        .fold(0, |c, p| if p.is_concealing() { c + 1 } else { c });
-    return result.to_string();
+        .fold(0, |c, p| if p.is_concealing() { c + 1 } else { c })
+        .to_string()
 }
 
 fn solve_second(input: &Vec<Pair>) -> String {
-    let result = input
+    input
         .iter()
-        .fold(0, |c, p| if p.is_intersecting() { c + 1 } else { c });
-    return result.to_string();
+        .fold(0, |c, p| if p.is_intersecting() { c + 1 } else { c })
+        .to_string()
 }
 
 pub fn solve(input: Vec<String>) -> (String, String) {
     let data = parse_input(input);
-    return (solve_first(&data), solve_second(&data));
+
+    (solve_first(&data), solve_second(&data))
 }
