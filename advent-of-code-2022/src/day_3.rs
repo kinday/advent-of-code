56a66421f56985293deb2efb567266356e4876d3
Leonard Kinday <leonard@kinday.ru>
2022-12-04 22:44:27 +0100

    Refactor 2022/3
    Use traits, clean up variables and return statements.

--- advent-of-code-2022/src/day_3.rs
+++ advent-of-code-2022/src/day_3.rs
@@ -1,91 +1,152 @@
-use std::collections::HashSet;
+struct Compartment {
+    value: String,
+}
 
-type Compartment = String;
 struct Rucksack(Compartment, Compartment);
 
-type Group = Vec<Compartment>;
+struct Rucksacks(Vec<Rucksack>);
+
+struct Group(Compartment, Compartment, Compartment);
+struct Groups(Vec<Group>);
 
 const ASCII_LOWERCASE_A: u32 = 97;
 const ASCII_LOWERCASE_Z: u32 = 122;
 const ASCII_UPPERCASE_A: u32 = 65;
 const ASCII_UPPERCASE_Z: u32 = 90;
 
-trait Intersectable {
-    fn intersection(&self) -> char;
+trait Priority {
+    fn as_priority(&self) -> u32;
+    fn to_priority(item: char) -> u32 {
+        let code = item as u32;
+
+        return match code {
+            (ASCII_LOWERCASE_A..=ASCII_LOWERCASE_Z) => code - ASCII_LOWERCASE_A + 1,
+            (ASCII_UPPERCASE_A..=ASCII_UPPERCASE_Z) => code - ASCII_UPPERCASE_A + 27,
+            _ => 0,
+        };
+    }
 }
 
-fn parse_line(line: &String) -> Rucksack {
-    let length = line.len();
-    let compartments = line.split_at(length / 2);
-    let compartment_a = compartments.0.to_string();
-    let compartment_b = compartments.1.to_string();
-    return Rucksack(compartment_a, compartment_b);
+impl From<&String> for Compartment {
+    fn from(s: &String) -> Self {
+        Compartment { value: s.clone() }
+    }
 }
 
-fn parse_input(lines: Vec<String>) -> Vec<Rucksack> {
-    return lines.iter().map(parse_line).collect();
-}
-
-fn find_common_item(rucksack: &Rucksack) -> char {
-    for a in rucksack.0.chars() {
-        for b in rucksack.1.chars() {
-            if a == b {
-                return b;
-            }
+impl From<&str> for Compartment {
+    fn from(s: &str) -> Self {
+        Compartment {
+            value: s.to_string(),
         }
     }
-
-    panic!("huh?")
 }
 
-fn to_priority(item: char) -> u32 {
-    let code = item as u32;
+impl From<&String> for Rucksack {
+    fn from(s: &String) -> Self {
+        let length = s.len();
+        let (a, b) = s.split_at(length / 2);
 
-    return match code {
-        (ASCII_LOWERCASE_A..=ASCII_LOWERCASE_Z) => code - ASCII_LOWERCASE_A + 1,
-        (ASCII_UPPERCASE_A..=ASCII_UPPERCASE_Z) => code - ASCII_UPPERCASE_A + 27,
-        _ => 0,
-    };
+        Rucksack(Compartment::from(a), Compartment::from(b))
+    }
 }
 
-fn solve_first(rucksacks: &Vec<Rucksack>) -> String {
-    let items: u32 = rucksacks
+impl Priority for Rucksack {
+    fn as_priority(&self) -> u32 {
+        for a in self.0.value.chars() {
+            for b in self.1.value.chars() {
+                if a == b {
+                    return Self::to_priority(b);
+                }
+            }
+        }
+
+        panic!("huh?")
+    }
+}
+
+impl From<&Vec<String>> for Rucksacks {
+    fn from(v: &Vec<String>) -> Self {
+        v.iter().map(|x| Rucksack::from(x)).collect()
+    }
+}
+
+impl FromIterator<Rucksack> for Rucksacks {
+    fn from_iter<T: IntoIterator<Item = Rucksack>>(iter: T) -> Self {
+        let mut v = Vec::new();
+        for item in iter {
+            v.push(item)
+        }
+
+        Rucksacks(v)
+    }
+}
+
+impl From<&[String]> for Group {
+    fn from(s: &[String]) -> Self {
+        let mut compartments = s.iter().map(|x| Compartment::from(x));
+
+        Group(
+            compartments.next().expect("no first compartment"),
+            compartments.next().expect("no second compartment"),
+            compartments.next().expect("no third compartment"),
+        )
+    }
+}
+
+impl From<&Vec<String>> for Groups {
+    fn from(v: &Vec<String>) -> Self {
+        v.chunks(3).map(|x| Group::from(x)).collect()
+    }
+}
+
+impl FromIterator<Group> for Groups {
+    fn from_iter<T: IntoIterator<Item = Group>>(iter: T) -> Self {
+        let mut v = Vec::new();
+        for item in iter {
+            v.push(item)
+        }
+
+        Groups(v)
+    }
+}
+
+impl Priority for Group {
+    fn as_priority(&self) -> u32 {
+        let mut ab = Vec::new();
+        for a in self.0.value.chars() {
+            for b in self.1.value.chars() {
+                if a == b {
+                    ab.push(a);
+                }
+            }
+        }
+
+        for c in self.2.value.chars() {
+            if ab.contains(&c) {
+                return Self::to_priority(c);
+            }
+        }
+
+        panic!("huh?")
+    }
+}
+
+fn solve_first(input: &Vec<String>) -> String {
+    Rucksacks::from(input)
+        .0
         .iter()
-        .map(find_common_item)
-        .map(to_priority)
-        .sum();
-    return items.to_string();
+        .fold(0, |sum, x| sum + x.as_priority())
+        .to_string()
 }
 
-fn parse_input_second(lines: Vec<String>) -> Vec<Group> {
-    return lines
-        .chunks(3)
-        .map(|x| x.iter().map(|x| x.clone()).collect())
-        .collect();
-}
-
-fn solve_second(groups: &Vec<Group>) -> String {
-    let items: u32 = groups
+fn solve_second(input: &Vec<String>) -> String {
+    Groups::from(input)
+        .0
         .iter()
-        .map(|group| {
-            group
-                .iter()
-                .map(|rucksack| rucksack.chars().collect::<HashSet<char>>())
-                .reduce(|x, y| x.intersection(&y).map(|x| x.to_owned()).collect())
-                .unwrap()
-                .iter()
-                .last()
-                .unwrap()
-                .clone()
-        })
-        .map(to_priority)
-        .sum();
-    return items.to_string();
+        .fold(0, |sum, x| sum + x.as_priority())
+        .to_string()
 }
 
 pub fn solve(input: Vec<String>) -> (String, String) {
-    // TODO: Get rid of double parsing
-    let data = parse_input(input.clone());
-    let data_second = parse_input_second(input);
-    return (solve_first(&data), solve_second(&data_second));
+    (solve_first(&input), solve_second(&input))
 }
