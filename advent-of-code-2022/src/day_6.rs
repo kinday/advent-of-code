7812ff5d1c4b942efb2385ee19cb9936bd9a4455
Leonard Kinday <leonard@kinday.ru>
2022-12-06 17:35:04 +0100

    Refactor 2022/6
    Get rid of traits, clean up the code.

--- advent-of-code-2022/src/day_6.rs
+++ advent-of-code-2022/src/day_6.rs
@@ -1,47 +1,35 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashSet, VecDeque};
 
-#[derive(Debug)]
-struct StartMarkerParseError;
+const START_OF_PACKET_MARKER_LENGTH: usize = 4;
+const START_OF_MESSAGE_MARKER_LENGTH: usize = 14;
 
-struct StartMarker {
-    value: usize,
-}
-
-impl std::str::FromStr for StartMarker {
-    type Err = StartMarkerParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // let mut char_index = HashMap::new();
-        // let mut char_buffer = VecDeque::with_capacity(4);
-        // for (i, c) in s.chars().enumerate() {
-        //   char_buffer.push_back(c);
-        //   char_index.insert(c, i);
-        //   char_buffer.iter().map(|c| char_index.get(&c).and_then()).fuse()
-        // }
-        let mut char_set = HashSet::with_capacity(4);
-        let mut char_buffer = VecDeque::with_capacity(5);
-        for x in s.chars().enumerate() {
-            char_buffer.push_back(x);
-            // TODO: Make parametrized
-            if char_buffer.len() > 14 {
-                char_buffer.pop_front();
-                char_set.clear();
-                if char_buffer.iter().map(|x| char_set.insert(x.1)).all(|x| x) {
-                    return Ok(StartMarker { value: x.0 + 1 });
-                }
+fn find_signal(length: usize, s: &str) -> Option<usize> {
+    let mut char_set = HashSet::with_capacity(4);
+    let mut char_buffer = VecDeque::with_capacity(5);
+    for x in s.chars().enumerate() {
+        char_buffer.push_back(x);
+        if char_buffer.len() > length {
+            char_buffer.pop_front();
+            char_set.clear();
+            if char_buffer.iter().map(|x| char_set.insert(x.1)).all(|x| x) {
+                return Some(x.0 + 1);
             }
         }
-
-        Err(StartMarkerParseError)
     }
+
+    None
 }
 
 fn solve_first(input: &String) -> String {
-    input.parse::<StartMarker>().unwrap().value.to_string()
+    find_signal(START_OF_PACKET_MARKER_LENGTH, input)
+        .unwrap()
+        .to_string()
 }
 
 fn solve_second(input: &String) -> String {
-    String::from("...")
+    find_signal(START_OF_MESSAGE_MARKER_LENGTH, input)
+        .unwrap()
+        .to_string()
 }
 
 pub fn solve(input: String) -> (String, String) {
